bee2a7ff4e412d56c3f64f93407174546f7d7c43
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2020-04-20 20:14:09 +0200

    BootstrapVue + Added register route, also included axios for api calls

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,7 @@
 <template>
-  <b-container>
-      <b-row>
-        <b-col>
-            1
-        </b-col>
-        <b-col>
-            2
-        </b-col>
-        <b-col>
-            3
-        </b-col>
-      </b-row>
-  </b-container>
+  <div>
+    NavBar
+  </div>
 </template>
 
 <script>
