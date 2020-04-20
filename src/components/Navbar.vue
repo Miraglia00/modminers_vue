26360de6eb7af5207a1b358802521ed96da30a3d
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2020-04-20 16:39:35 +0200

    Vue+Router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-container>
+      <b-row>
+        <b-col>
+            1
+        </b-col>
+        <b-col>
+            2
+        </b-col>
+        <b-col>
+            3
+        </b-col>
+      </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
